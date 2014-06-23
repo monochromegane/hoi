86084ff2fb1f75b124cd1986d72f0cc74246b63a
monochromegane <dev.kuro.obi@gmail.com>
2014-06-23 22:39:33 +0900

    Initial commit.

---
+++ main.go
@@ -0,0 +1,40 @@
+package main
+
+import (
+	"crypto/rand"
+	"fmt"
+	"os"
+	"os/user"
+	"path/filepath"
+)
+
+func main() {
+
+	file := os.Args[1]
+
+	// create hoi public directory
+	usr, _ := user.Current()
+	homeDir := usr.HomeDir
+	publicDir := filepath.Join(homeDir, ".hoi", "public")
+	os.MkdirAll(publicDir, 0755)
+
+	// create random directory
+	random := randomString(32)
+	randomDir := filepath.Join(publicDir, random)
+	os.Mkdir(randomDir, 0755)
+
+	// create symblic link
+	os.Symlink(file, filepath.Join(randomDir, filepath.Base(file)))
+
+	fmt.Println(filepath.Join(random, filepath.Base(file)))
+}
+
+func randomString(length int) string {
+	alphanum := "0123456789abcdefghijklmnopqrstuvwxyz"
+	var bytes = make([]byte, length)
+	rand.Read(bytes)
+	for i, b := range bytes {
+		bytes[i] = alphanum[b%byte(len(alphanum))]
+	}
+	return string(bytes)
+}
