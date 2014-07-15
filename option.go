05c92729994c66ac74372dcdefa144b5d70abf9e
monochromegane <dev.kuro.obi@gmail.com>
2014-07-15 21:11:33 +0900

    Changed clear option description.

--- option.go
+++ option.go
@@ -2,6 +2,6 @@ package hoi
 
 type Options struct {
 	Server  bool `short:"s" long:"server" description:"Start hoi server."`
-	Clear   bool `short:"c" long:"clear" description:"Clear all symlinks by removing public directory."`
+	Clear   bool `short:"c" long:"clear" description:"Clear all symlinks by removing contents under public directory."`
 	Version bool `long:"version" description:"Show version"`
 }
